e3f3824d7c162a62e40d95f7deb89bbe4355e4d6
Mukil Loganathan <mukilloganathan@gmail.com>
2018-09-26 00:12:32 -0700

    Added devise boilerplate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(user)
+    case
+    when user.is_a?(Admin)
+      admins_path
+    when user.is_a?(CommunityLeader)
+      community_leaders_path
+    else
+      volunteers_path
+    end
+  end
 end
---
+++ app/controllers/community_leaders_controller.rb
@@ -0,0 +1,2 @@
+class CommunityLeadersController < ApplicationController
+end
