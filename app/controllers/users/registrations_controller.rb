9673430280b048f762b760af9596f5eeb772d534
Mukil Loganathan <mukilloganathan@gmail.com>
2018-10-15 23:00:54 -0700

    make sure role is persisted to db

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_user, except: [:new, :create]
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -44,7 +44,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
