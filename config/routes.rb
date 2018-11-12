569a0d291a04a47425acad626c97e97fe54bf5ca
Peter Veerman <peterkangveerman@gmail.com>
2018-11-12 15:59:35 -0800

    Project Skeleton (#15)
    * Add dashboard view
    
    * Write skeleton views and controller out for projects
    
    Created a new non-api projects controller to handle
    project views. Created skeleton views for projects.
    Made react component for the form to create a new
    project.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users, :projects
 
   authenticated :user do
     root 'users#dashboard', as: :authenticated_user_root
