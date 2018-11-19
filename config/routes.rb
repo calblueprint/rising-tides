42e38fa11b51037d11edef19a79a2530b1da4a1a
Steve Shi <steveshi@berkeley.edu>
2018-11-18 16:57:58 -0800

    Added routes and applications model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/new'
-  get 'pages/create'
   devise_for :organizations
   devise_for :users, path: 'users', controllers: {
       sessions: 'users/sessions',
@@ -8,14 +6,25 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, :projects
+  resources :applications # To be deleted, currently just for testing
+  resources :users, only: [:edit]
+  resources :organizations, only: [:edit]
+
+  resources :projects, only: [:index, :show, :edit, :apply] do
+    resources :applications, only: [:index, :show, :edit, :delete]
+  end
 
   authenticated :user do
     root 'users#dashboard', as: :authenticated_user_root
   end
 
+  authenticated :organization do
+    root 'organizations#dashboard', as: :authenticated_organization_root
+  end
+
   namespace :api, defaults: { format: :json } do
-    resources :projects, :only => [:index, :show, :create, :update, :destroy]
+    resources :projects, only: [:index, :show, :create, :update, :destroy]
+    resources :applications, only: [:index, :show, :create, :update, :destroy]
   end
 
   root 'pages#dashboard'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_093417) do
+ActiveRecord::Schema.define(version: 2018_11_18_234519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string "email", default: "", null: false
