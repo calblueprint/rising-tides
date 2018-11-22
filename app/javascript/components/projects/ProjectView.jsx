31bf67bcfb4718d94e4e38c956f9e5478d8548c1
Peter Veerman <peterkangveerman@gmail.com>
2018-11-21 16:44:54 -0800

    Get rid of unnecessary comments

--- app/javascript/components/projects/ProjectView.jsx
+++ app/javascript/components/projects/ProjectView.jsx
@@ -7,24 +7,6 @@ import axios from 'axios';
 
 class ProjectView extends React.Component {
 
-  // constructor(props) {
-  //   super();
-  //   this.state = {
-  //     project: null
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   axios.get('/api/projects/' + this.props.project.id)
-  //     .then(res => {
-  //       this.setState({ project: res.data });
-  //       console.log(this.state);
-  //     })
-  //     .catch(res => {
-  //       console.log("ERROR" + res.data);
-  //     });
-  // }
-
   render() {
     const { project } = this.props;
 
@@ -34,10 +16,6 @@ class ProjectView extends React.Component {
           <p>{project.description}</p>
       </div>
     );
-
-    // return (
-    //   <div></div>
-    // );
   }
 }
 
