9b6ec13d5de6665eb8c5c2a97677a0e1a015c1e7
Kyu Won Kim <freddy.kim8@gmail.com>
2019-02-23 05:50:19 -0800

    File updates

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 2018_12_01_221552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "applications", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,8 +34,20 @@ ActiveRecord::Schema.define(version: 2018_12_01_221552) do
     t.text "question2"
     t.text "question3"
     t.bigint "project_id"
-    t.integer "user_id"
-    t.integer "status"
+    t.integer "status", default: 0
+    t.bigint "user_id"
+  end
+
+  create_table "community_leaders", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_community_leaders_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_community_leaders_on_reset_password_token", unique: true
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -82,6 +106,18 @@ ActiveRecord::Schema.define(version: 2018_12_01_221552) do
     t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "volunteers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_volunteers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_volunteers_on_reset_password_token", unique: true
   end
 
 end
