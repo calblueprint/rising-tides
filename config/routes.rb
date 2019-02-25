fdd494d6b6a1bbddac4ddf2710e741cd50264063
Kyu Won Kim <freddy.kim8@gmail.com>
2019-02-24 21:43:51 -0800

    Add admin account
    Added devise admin account for easier viewing of all the accounts in the database.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :organizations, controllers: {
       sessions: 'organizations/sessions',
       registrations: 'organizations/registrations',
