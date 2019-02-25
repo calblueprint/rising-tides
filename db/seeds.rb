8b9053830d51120772e85a1b07c15e50c6d98e92
Kyu Won Kim <freddy.kim8@gmail.com>
2019-02-24 20:23:39 -0800

    Start Seeding DB
    Added Faker gem for random generation of data fields.
    Began adding seed data for Users and Orgs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Constants
+NUM_USERS = 20
+NUM_ORGANIZATIONS = 10
+NUM_PROJECTS = 5
+NUM_APPLICATIONS = NUM_PROJECTS * 2
+
+# Use Faker gem to randomly generate fields
+require 'faker'
+
+def make_users
+  1.upto(NUM_USERS) do |i|
+    user = User.create!(email: "user#{i}@gmail.com",
+                 password: 'password',
+                 first_name: Faker::Name.first_name,
+                 last_name: Faker::Name.last_name,
+                 city: Faker::Address.city,
+                 state: Faker::Address.state_abbr,
+                 bio: Faker::RickAndMorty.quote,
+                 phone_number: Faker::PhoneNumber.phone_number)
+    user.save
+
+  end
+end
+10.times do
+  password = Faker::Internet.password
+  User.create!(email: Faker::Internet.email,
+               password: password,
+               first_name: Faker::Name.first_name,
+               last_name: Faker::Name.last_name,
+               city: Faker::Address.city,
+               state: Faker::Address.state_abbr,
+               bio: Faker::RickAndMorty.quote,
+               phone_number: Faker::PhoneNumber.phone_number)
+end
+
+5.times do
+  password = Faker::Internet.password
+  User.create!(email: Faker::Internet.email,
+               password: password,
+               name: Faker::Company.name,
+               city: Faker::Address.city,
+               state: Faker::Address.state_abbr,
+               description: Faker::MichaelScott.quote)
+end
