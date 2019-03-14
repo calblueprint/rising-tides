316d2e0e454cb6d766a7311fb2048e4f3246d1af
Kyu Won Kim <freddy.kim8@gmail.com>
2019-03-14 05:45:00 -0700

    attempt 1 at fixing babel

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,18 @@
 const {environment} = require('@rails/webpacker');
-
 const webpack = require('webpack');
+
+environment.loaders.prepend('eslint', {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: [{loader: 'eslint-loader'}]
+})
+
+environment.loaders.prepend('babel-loader', {
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  use: [{loader: 'babel-loader'}]
+})
+
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery'
