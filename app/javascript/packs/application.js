47bbd12852601658c1b4c1656e489d3fb7c89a53
Kyu Won Kim <freddy.kim8@gmail.com>
2019-03-14 18:32:42 -0700

    Prepare to add react-router

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import "@babel/polyfill";
 
 console.log('Hello World from Webpacker')
 // Support component names relative to this directory:
