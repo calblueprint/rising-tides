402eb4faefb2ccbc3d00f4dcbcc13150d9ff67d3
Peter Veerman <peterkangveerman@gmail.com>
2019-03-15 03:51:39 -0700

    Add photos fixes (#46)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_002915) do
+ActiveRecord::Schema.define(version: 2019_03_10_215130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,12 @@ ActiveRecord::Schema.define(version: 2019_03_03_002915) do
     t.bigint "project_id"
   end
 
+  create_table "project_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -88,6 +94,7 @@ ActiveRecord::Schema.define(version: 2019_03_03_002915) do
     t.text "question1"
     t.text "question2"
     t.text "question3"
+    t.bigint "project_type_id"
   end
 
   create_table "users", force: :cascade do |t|
