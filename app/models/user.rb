38ff65e55faf828cd2b84696c058bdb83ff36ddd
Peter Veerman <peterkangveerman@gmail.com>
2019-03-15 17:07:58 -0700

    Add skills to users (#58)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :applications
+  has_and_belongs_to_many :skills
 
   has_attached_file :profile_image
   validates_attachment_content_type :profile_image, content_type: /\Aimage\/.*\z/
