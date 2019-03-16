03af230f9c93eaf767007808731d869ac52ddc02
Peter Veerman <peterkangveerman@gmail.com>
2019-03-15 19:19:12 -0700

    Correct schema
    Fix merge conflict
    
    Correct schema

--- db/schema.rb
+++ db/schema.rb
@@ -38,12 +38,6 @@ ActiveRecord::Schema.define(version: 2019_03_16_013734) do
     t.bigint "user_id"
   end
 
-  create_table "deliverable_types", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "organizations", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -78,21 +72,6 @@ ActiveRecord::Schema.define(version: 2019_03_16_013734) do
     t.bigint "project_id"
   end
 
-  create_table "project_skill_assignments", force: :cascade do |t|
-    t.bigint "project_id"
-    t.bigint "skill_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_project_skill_assignments_on_project_id"
-    t.index ["skill_id"], name: "index_project_skill_assignments_on_skill_id"
-  end
-
-  create_table "project_statuses", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "project_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -116,10 +95,13 @@ ActiveRecord::Schema.define(version: 2019_03_16_013734) do
     t.text "question2"
     t.text "question3"
     t.bigint "project_type_id"
+<<<<<<< HEAD
     t.bigint "project_status_id", default: 1
     t.bigint "deliverable_type_id"
     t.integer "application_limit", default: 2
     t.integer "user_limit", default: 1
+=======
+>>>>>>> aa54f8b... Correct schema
   end
 
   create_table "projects_skills", id: false, force: :cascade do |t|
@@ -167,6 +149,4 @@ ActiveRecord::Schema.define(version: 2019_03_16_013734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "project_skill_assignments", "projects"
-  add_foreign_key "project_skill_assignments", "skills"
 end
