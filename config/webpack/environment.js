fcfa4890bf76d25288ba686be6353c780d977b29
Kyu Won Kim <freddy.kim8@gmail.com>
2019-03-17 15:49:16 -0700

    Remove eslint from webpack compile
    Eslint checking during webpack compiling creates an overlay when using webpack-dev-server

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,14 @@
-const {environment} = require('@rails/webpacker');
+const { environment } = require("@rails/webpacker");
 
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery'
-}));
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  })
+);
 
 environment.loaders.prepend("babel-loader", {
   test: /\.jsx$|\.js$/,
@@ -13,10 +16,10 @@ environment.loaders.prepend("babel-loader", {
   use: [{ loader: "babel-loader" }]
 });
 
-environment.loaders.prepend("eslint", {
-  test: /\.jsx$|\.js$/,
-  exclude: /node_modules/,
-  use: [{ loader: "eslint-loader" }]
-});
+// environment.loaders.prepend("eslint", {
+//   test: /\.jsx$|\.js$/,
+//   exclude: /node_modules/,
+//   use: [{ loader: "eslint-loader" }]
+// });
 
 module.exports = environment;
