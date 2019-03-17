8d8b95c04ec320dfcebedd3108821d509964398d
Peter Veerman <peterkangveerman@gmail.com>
2019-03-16 17:31:27 -0700

    Deliverable types - Backend (#54) (#55)
    * Add skills to users (#58)
    
    Add skills
    
    Make skill join table
    
    Add API skill filtering
    
    Add project controller tests
    
    Add skill filtering test
    
    Use has_and_belongs_to_many and use scopes
    
    Change skill seeds
    
    Remove old join table
    
    Add skill create with integreation test
    
    Fix skills controller
    
    Add deliverable type to project
    
    Add filtering for deliverable types and tests
    
    Add deliverable types create method and integration test
    
    * Update projects_controller.rb
    
    * Update project.rb
    
    * Delete project_skill_assignment.rb
    
    * Delete 20190315161216_create_project_skill_assignments.rb
    
    * Update seeds.rb
    
    * Update seeds.rb
    
    * Delete project_skill_assignments.yml
    
    Add skills to users (#58)
    
    Add skills
    
    Make skill join table
    
    Add API skill filtering
    
    Add project controller tests
    
    Add skill filtering test
    
    Make max limits for accepted volunteers and number of applications
    
    Add max limits to applications and accepted users
    
    Rename max_applications to application_limit and max_users to user_limit
    
    Change decision to enum
    
    Add deliverable types to schema

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,14 @@ ActiveRecord::Schema.define(version: 2019_03_16_013734) do
     t.text "question2"
     t.text "question3"
     t.bigint "project_id"
-    t.integer "status", default: 0
-    t.bigint "user_id"
+    t.integer "user_id"
+    t.integer "status"
+  end
+
+  create_table "deliverable_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -72,6 +78,15 @@ ActiveRecord::Schema.define(version: 2019_03_16_013734) do
     t.bigint "project_id"
   end
 
+  create_table "project_skill_assignments", force: :cascade do |t|
+    t.bigint "project_id"
+    t.bigint "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_skill_assignments_on_project_id"
+    t.index ["skill_id"], name: "index_project_skill_assignments_on_skill_id"
+  end
+
   create_table "project_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -95,10 +110,9 @@ ActiveRecord::Schema.define(version: 2019_03_16_013734) do
     t.text "question2"
     t.text "question3"
     t.bigint "project_type_id"
-    t.bigint "project_status_id", default: 1
-    t.bigint "deliverable_type_id"
     t.integer "application_limit", default: 2
     t.integer "user_limit", default: 1
+    t.bigint "deliverable_type_id"
   end
 
   create_table "projects_skills", id: false, force: :cascade do |t|
@@ -146,4 +160,6 @@ ActiveRecord::Schema.define(version: 2019_03_16_013734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "project_skill_assignments", "projects"
+  add_foreign_key "project_skill_assignments", "skills"
 end
