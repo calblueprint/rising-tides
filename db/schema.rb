a7661ad49bc8a2d363e732d0872544e93b17f7ec
Kyu Won (Frederick) Kim <freddy.kim8@gmail.com>
2019-03-20 22:19:27 -0700

    Setup webpack-dev-server (#65)
    * Setup webpack-dev-server
    
    * Initialized workspace by auto-updating db schema
    * Updated webpack-dev-server
    * Added webpack to yarn list
    * Removed eslint from webpack compile
    * Made HMR to true for future hot reloading functionality
    * Added "poll:1000" to have webpack to know when a file has been updated
    
    Update docker-compose to use webpack-dev-server
    
    Also, added  a dockerignore to save space
    
    Update db schema
    
    Update webpack-dev-server
    
    Remove eslint from webpack compile
    
    Eslint checking during webpack compiling creates an overlay when using webpack-dev-server
    
    Turn on hot reloading for webpack-dev-server
    
    Changed hmr to true to use hot reloading
    
    Setup webpack-dev-server
    
    Added 'webpack' to package.json.
    Added "poll: 1000" so that webpack can know if a file has been changed.
    
    * Change for local development
    
    * Changed webpack-dev-server to localhost rather than sh
    * Removed commented out eslint in environment.js

--- db/schema.rb
+++ db/schema.rb
@@ -116,11 +116,6 @@ ActiveRecord::Schema.define(version: 2019_03_17_004333) do
     t.text "question2"
     t.text "question3"
     t.bigint "project_type_id"
-    t.integer "application_limit", default: 2
-    t.integer "user_limit", default: 1
-    t.bigint "deliverable_type_id"
-    t.bigint "project_status_id", default: 1
-    t.integer "status"
   end
 
   create_table "projects_skills", id: false, force: :cascade do |t|
