757f61e7d1bf1303c7304e791d1c28682b3ae815
Kyu Won (Frederick) Kim <freddy.kim8@gmail.com>
2019-03-20 22:19:27 -0700

    Setup webpack-dev-server (#65)
    * Setup webpack-dev-server
    
    * Initialized workspace by auto-updating db schema
    * Updated webpack-dev-server
    * Added webpack to yarn list
    * Removed eslint from webpack compile
    * Made HMR to true for future hot reloading functionality
    * Added "poll:1000" to have webpack to know when a file has been updated
    
    Update docker-compose to use webpack-dev-server
    
    Also, added  a dockerignore to save space
    
    Update db schema
    
    Update webpack-dev-server
    
    Remove eslint from webpack compile
    
    Eslint checking during webpack compiling creates an overlay when using webpack-dev-server
    
    Turn on hot reloading for webpack-dev-server
    
    Changed hmr to true to use hot reloading
    
    Setup webpack-dev-server
    
    Added 'webpack' to package.json.
    Added "poll: 1000" so that webpack can know if a file has been changed.
    
    * Change for local development
    
    * Changed webpack-dev-server to localhost rather than sh
    * Removed commented out eslint in environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,22 +1,19 @@
-const {environment} = require('@rails/webpacker');
+const { environment } = require("@rails/webpacker");
 
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery'
-}));
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  })
+);
 
 environment.loaders.prepend("babel-loader", {
   test: /\.jsx$|\.js$/,
   exclude: /node_modules/,
   use: [{ loader: "babel-loader" }]
-});
-
-environment.loaders.prepend("eslint", {
-  test: /\.jsx$|\.js$/,
-  exclude: /node_modules/,
-  use: [{ loader: "eslint-loader" }]
 });
 
 module.exports = environment;
