cc71e87846cf158e68a1e07f645d99be29d09bea
Peter Veerman <peterkangveerman@gmail.com>
2019-04-07 22:47:22 -0700

    Redesign profile header

--- app/javascript/components/volunteer/Profile.jsx
+++ app/javascript/components/volunteer/Profile.jsx
@@ -33,37 +33,35 @@ class Profile extends React.Component {
     let profileUrl = this.props.profile_image_url ? this.props.profile_image_url : "https://media.licdn.com/dms/image/C4E03AQFbjc-XoDAJtA/profile-displayphoto-shrink_200_200/0?e=1559779200&v=beta&t=zCNkokfNKlZr1fjfa-ztpX7dMsji-hUfPYu21S7Qhzg";
     let profileImage = <img className="h-100 w4"  src={profileUrl} />;
     let resumeUrl = this.props.resume_url ? this.props.resume_url : "";
-    let resume = <a className="fl pa0 pv0 ph3 resume-button f7 lh-m" src={resumeUrl}>resume</a>;
+    let resume = <a className="dib std-button f7 lh-m ml3" src={resumeUrl}>resume</a>;
     return (
         <div className="w-100 h-100 tc">
             <div className="tl fl w-100 pl6 pr6 pt5 pb5">
-                <div className="h4 flex">
+                <div className="h4 flex items-end">
                     {profileImage}
-                    <div className="w-100 h-100 m3 ph4 pt4">
-                        <div className="h2 f1">
-                            {this.props.user.first_name} {this.props.user.last_name}
+                    <div className="w-100 m3 ph4 pt4">
+                        <div className="mb3">
+                            <h1 className="ma0 f1 dib">
+                                {this.props.user.first_name} {this.props.user.last_name}
+                            </h1>
+                            <a className="dib pa0 ph1 ml3" target="_blank" href={`http://${this.props.user.link}`}>
+                                <img src="/images/linkedin-icon.png" style={{ width: '21px', height: '21px' }} />
+                            </a>
+                            {resume}
                         </div>
-                        <hr />
-                        <div className="h1 flex">
-                            <div className="ph3 mt1-ns">
-                                <h3 className="f7 ma0 light-grey">Email</h3>
+                        <div className="flex">
+                            <div className="mt1-ns">
                                 {this.props.user.email}
                             </div>
-                            <div className="ph3 mt1-ns">
-                                <h3 className="f7 ma0 light-grey">Phone number</h3>
+                            <span className="pl3 mt1-ns">|</span>
+                            <div className="pl3 mt1-ns">
                                 {this.props.user.phone_number}
                             </div>
-                            <div className="ph3 mt1-ns">
-                                <h3 className="f7 ma0 light-grey">Location</h3>
+                            <span className="pl3 mt1-ns">|</span>
+                            <div className="pl3 mt1-ns">
                                 {this.props.user.city}, {this.props.user.state}
                             </div>
                         </div>
-                    </div>
-                    <div className="h-100 w3 w-20">
-                        <a className="fl pa0 ph1 mr2" target="_blank" href={`http://${this.props.user.link}`}>
-                            <img src="/images/linkedin-icon.png" style={{ width: '21px', height: '21px' }} />
-                        </a>
-                        {resume}
                     </div>
                 </div>
 
@@ -74,9 +72,6 @@ class Profile extends React.Component {
                 {this.props.user.bio}
 
                 <h3 className="pt5">Projects</h3>
-
-                <br />
-                <a onClick={this.goBack}>Back</a>
             </div>
         </div>
     );
