9bfc5dfb53e8cf201c91dc9d3b8f7d5cd99138f7
aliceyezhao <zhaotoo@gmail.com>
2019-04-08 17:14:57 -0700

    updated navbar to figma

--- app/javascript/components/utils/NavBar.jsx
+++ app/javascript/components/utils/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "images/rising-tides-logo.png";
+import PropTypes from "prop-types";
 
 
 class NavBar extends React.Component {
@@ -16,7 +17,7 @@ class NavBar extends React.Component {
   };
 
   goToProfile = () => {
-    window.location.href = `/users/${this.props.id}`;
+    window.location.href = `/users/${this.props.user.id}`;
   };
 
   goToBrowse = () => {
@@ -28,10 +29,14 @@ class NavBar extends React.Component {
     <img className="fl h3 w-auto" alt="The Rising Tides Logo" src={logo} onClick={this.goToDashboard} />
     <ul className="ul">
       <li className="fr f4 w4 tc"> 
+        <a className="f4 no-link black hover-black" onClick={this.goToProfile}>{this.props.user.first_name}</a>
+      </li>
+      <li className="fr f4 w4 tc"> 
         <a className="f4 no-link black hover-black" onClick={this.goToBrowse}>Search</a>
       </li>
-      <li className="link fr f4 w4 tc" onClick={this.goToDashboard}>Dashboard</li>
-      <li className="link fr f4 w4 tc" onClick={this.goToProfile}>Profile</li>
+      <li className="fr f4 w4 tc"> 
+        <a className="b f4 no-link black hover-black" onClick={this.goToDashboard}>Dashboard</a>
+      </li>
     </ul>
     <br/><br/>
     <hr className="hr-navbar fr pr5"/>
@@ -39,5 +44,8 @@ class NavBar extends React.Component {
   }
 }
 
+NavBar.propTypes = {
+  user: PropTypes.object,
+};
 
 export default NavBar;
