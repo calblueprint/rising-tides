4c767b2a18002e4733a7b249c19659fef9bb5dd3
aliceyezhao <zhaotoo@gmail.com>
2019-04-19 21:31:00 -0700

    buttons not ready

--- app/javascript/components/applications/ApplicationView.jsx
+++ app/javascript/components/applications/ApplicationView.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import axios from 'axios';
 import PropTypes from "prop-types";
 import linkedin from "images/linked-in.png";
-import FlashMessage from '../utils/FlashMessage';
 
 class ApplicationView extends React.Component {
     constructor(props) {
@@ -84,7 +83,6 @@ class ApplicationView extends React.Component {
     displayButtons() {
         let buttons = null;
         if (this.props.organization) {
-            console.log('org signed in');
             if (this.props.application.status === null || this.props.application.status === "pending") {
                 buttons = (
                     <div>
@@ -93,9 +91,8 @@ class ApplicationView extends React.Component {
                     </div>
                 );
             } else if (this.props.application.status == "interviewing") {
-                console.log('interviewing');
                 buttons = (
-                    <div>
+                    <div className="mv4">
                     <button onClick={this.handleAccept}>Accept</button>
                     <button onClick={this.handleDeny}>Reject</button>
                     </div>
@@ -108,23 +105,22 @@ class ApplicationView extends React.Component {
     render() {
       let profileUrl = this.props.profile_image_url ? this.props.profile_image_url : "https://media.licdn.com/dms/image/C4E03AQFbjc-XoDAJtA/profile-displayphoto-shrink_200_200/0?e=1559779200&v=beta&t=zCNkokfNKlZr1fjfa-ztpX7dMsji-hUfPYu21S7Qhzg";
       let profileImage = <img className="h-100 w4"  src={profileUrl} />;
-      let resume = <a className="dib std-button f7 pa1 lh-m">Resume</a>
+      let resume = <a className="dib std-button w3 f7 pa1 lh-m">Resume</a>
       return(
         <div className="w-100 h-100 tc">
-           <FlashMessage onRef={ref => (this.flash_message = ref)} />
-           <div
-                className="h5 absolute w-100 bg-black bg-image"
-                style={{zIndex: -1}} />
-            <div className="tl fl w-75 ml6 mr6 mt6 mb5 bg-white pa5">
-                <h1 className="ma0 f1 mb5 truncate"> Application - {this.props.project.title} </h1>
-                <button>{this.props.application.status}</button>
+            <div className="tl fl w-100 pl6 pr6 pt5 pb5">
+                <h1 className="ma0 f1 mb4 truncate"> Application - {this.props.project.title} </h1>
+                <div className="f5 pa2 w4 bg-accent mb4">
+                    {this.props.application.status}
+                </div>
                 <div className="h4 flex items-end">
                     {profileImage}
                     <div className="w-100 m3 ph4 pt4">
                         <div className="flex items-end">
-                            <h1 className="ma0 f1 mb3" onClick={this.handleVolunteerClick}>
+                            <h1 className="ma0 f1 mb3">
                                 {this.props.user.first_name} {this.props.user.last_name}
                             </h1>
+                            <br/>
                             <a className="pa0 ph1 ml3 mb1" href={`http://${this.props.user.linkedin_url}`}>
                                 <img src={linkedin} style={{ width: '21px', height: '21px' }} />
                             </a>
