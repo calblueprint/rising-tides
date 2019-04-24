5ffd79b5fa8fa6040adf791e93b7bce592f6e1a1
aliceyezhao <zhaotoo@gmail.com>
2019-04-23 20:42:40 -0700

    truncate proj name

--- app/javascript/components/utils/ProjectCard.jsx
+++ app/javascript/components/utils/ProjectCard.jsx
@@ -14,7 +14,7 @@ class ProjectCard extends React.Component {
     return (<div className="proj-card col-item fl ma2 shadow-1" onClick={this.goToProject}>
               <img src="http://placekitten.com/g/600/300" className="db pic-height w-100 br2 br--top" alt="Photo of a kitten looking menacing."/>
               <div className="bt b--white pa3">
-                <p className="f4 b lato compact mt3 ma2">
+                <p className="f4 b lato truncate mt3 ma2">
                   {this.props.project.title}
                 </p>
                 <p className="f5 lato fl truncate mb0 ma2">
