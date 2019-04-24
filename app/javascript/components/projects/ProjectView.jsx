71cefa33477a934e95086313d20cd9d7740e3cb9
aliceyezhao <zhaotoo@gmail.com>
2019-04-24 00:03:13 -0700

    carousel comments

--- app/javascript/components/projects/ProjectView.jsx
+++ app/javascript/components/projects/ProjectView.jsx
@@ -5,6 +5,7 @@
 import React from "react";
 import axios from "axios";
 import ApplicationList from '../applications/ApplicationList';
+import profile_pic from "images/profile_pic.png"
 
 class ProjectView extends React.Component {
   constructor(props) {
@@ -46,6 +47,64 @@ class ProjectView extends React.Component {
     window.location.href = "/projects";
   };
 
+//   plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
+  
+//   currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
+  
+//   showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {slideIndex = 1}    
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";  
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";  
+//     dots[slideIndex-1].className += " active";
+//   }
+
+//   photoCarousel() {
+//     let photo_carousel = null;
+//     //to be replaced by if (this.props.project.photos) but no photos currently
+//     if (1) {
+//         photo_carousel = (
+//         <div class="w-70">
+//             <div class="mySlides fade">
+//                 <img src="images/proj-photo.jpg" style="width:100%"/>
+//             </div>      
+//                 <div class="mySlides fade">
+//                 <img src="images/proj-photo.jpg" style="width:100%"/>
+//             </div>
+            
+//             <div class="mySlides fade">
+//                 <img src="images/proj-photo.jpg" style="width:100%"/>
+//             </div>
+            
+//             <a onclick={this.plusSlides(-1)}><i className="fa fa-chevron-left f4"></i></a>
+//             <a onclick={this.plusSlides(1)}><i className="fa fa-chevron-right f4"></i></a>
+        
+//         </div>)
+//     }
+//     return photo_carousel;
+//   }
+
+//   carouselDots() {
+//       let carousel_dots = null;
+//       //to be replaced by if (this.props.project.photos) but no photos currently
+//       if (1) {
+
+//       }
+//       return carousel_dots;
+//   }
+
   render() {
     const { 
         project,
@@ -55,10 +114,19 @@ class ProjectView extends React.Component {
         milestones
     } = this.props;
 
+    // let carousel_dots = (<div style="text-align:center">
+    //                         <span class="dot" onclick="currentSlide(1)"></span> 
+    //                         <span class="dot" onclick="currentSlide(2)"></span> 
+    //                         <span class="dot" onclick="currentSlide(3)"></span> 
+    //                     </div>)
+
     var org_img = <span></span>;
     if (this.props.org_image_url) {
         org_img = <img className="w2 h2 mr2 dib"
                        src={this.props.org_image_url} />;
+    } else {
+        org_img = <img className="w2 h2 mr2 dib"
+                       src={profile_pic} />;
     }
 
     var project_string_status = "Accepting Applications";
@@ -87,6 +155,55 @@ class ProjectView extends React.Component {
     );
     if (!organization_signed_in || organization.id != current_organization.id) {
         edit_button = <span></span>;
+    }
+
+    let applicationList;
+
+    if (this.props.applications) {
+      applicationList = this.props.applications.map((application, index) => {
+        var project_status = (
+            <div className="dib rt-yellow-bg ph3 pv2 fw4">
+                Pending
+            </div>
+        );
+        if (application.status == "interviewing") {
+            project_status = (
+                <div className="dib rt-yellow-bg ph3 pv2 fw4">
+                    Interviewing
+                </div>
+            );
+        } else if (application.status == "accepted") {
+            project_status = (
+                <div className="dib accepted ph3 pv2 fw4">
+                    Accepted
+                </div>
+            );
+        } else if (application.status == "denied") {
+            project_status = (
+                <div className="dib ph3 pv2 fw4">
+                    No longer in consideration
+                </div>
+            );
+        }
+        return (
+            <div className="">
+                <div className="bt b--black-10" />
+                <div className="flex items-center pv3" key={index}>
+                    <h4 className="w-25 ma0">{application.project.title}</h4>
+                    <div className="w-25">
+                        {project_status}
+                    </div>
+                    <div className="w-25">{application.user.first_name} {application.user.last_name}</div>
+                    <a
+                        className="w-25 tr"
+                        href={"/applications/" + application.id}
+                        >View job description <span className="ml3 f5 fa fa-angle-right"></span></a>
+                </div>
+            </div>
+        );
+      });
+    } else {
+      applicationList = <div>No Results</div>;
     }
 
     let applications;
@@ -123,8 +240,8 @@ class ProjectView extends React.Component {
             <div className="tl fl w-75 ml6 mr6 mt6 mb5 bg-white pa5">
                 <h1 className="f1 ma0">{project.title}</h1>
                 {edit_button}
-                <div className="dib rt-yellow-bg ph3 pv2 mv4 f5 fw4">
-                    {project_string_status}
+                <div className="dib ba ph3 pv2 mv4 f5 fw4">
+                    {project_string_status} {apply_button}
                 </div>
                 <h3 className="mt3">Project Overview</h3>
                 <p>{project.overview}</p>
@@ -138,18 +255,17 @@ class ProjectView extends React.Component {
                     <div className="w-auto bg-light-gray pa3 h-auto">
                         <div className="flex items-center">
                             {org_img}
-                            <p className="ma0 f3 truncate dib">{organization.name}</p>
+                            <a className="ma0 f3 truncate dib">{organization.name}</a>
                         </div>
-                        <div className="mt2">
-                            <p className="fa lato f5 fa-map-pin truncate mr2">{organization.city}, {organization.state}</p>
+                        <div className="mt3 truncate">
+                            <i className="fas fa-map-pin f4"></i><span className="ml2 f5 truncate">{organization.city}, {organization.state}</span>
                         </div>
-                        <div className="mt2">
-                            <span className="fa lato f5 fa-calendar mr2">{project.start_time} - {project.end_time}</span>
+                        <div className="mt4 truncate">
+                            <i className="fas fa-calendar f4"></i><span className="ml2 f5 truncate">{project.start_time} - {project.end_time}</span>
                         </div>
-                        <div className="mt2 mb3">
-                            <span className="fa lato f5 fa-phone mr2">{organization.contact_phone_number}</span>
+                        <div className="mt4 truncate">
+                            <i className="fas fa-phone f4"></i><span className="ml2 f5 truncate">{organization.contact_phone_number}</span>
                         </div>
-                        {apply_button}
                     </div>
                 </div>
                 <h3 className="mt4">Project Description</h3>
