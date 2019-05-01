6b047aed9e9b507aa765805c574a4a5d0d97d57f
aliceyezhao <zhaotoo@gmail.com>
2019-04-30 20:49:26 -0700

    photo

--- app/javascript/components/organization/Profile.jsx
+++ app/javascript/components/organization/Profile.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import axios from "axios";
 import ProjectCard from '../utils/ProjectCard';
 import FlashMessage from '../utils/FlashMessage'
+import profile_pic from "images/profile_pic.png";
+
 
 class Profile extends React.Component {
   constructor(props) {
@@ -46,7 +48,7 @@ class Profile extends React.Component {
 
   render() {
     const { organization } = this.state;
-    let profileUrl = this.props.profile_image_url ? this.props.profile_image_url : "https://media.licdn.com/dms/image/C4E03AQFbjc-XoDAJtA/profile-displayphoto-shrink_200_200/0?e=1559779200&v=beta&t=zCNkokfNKlZr1fjfa-ztpX7dMsji-hUfPYu21S7Qhzg";
+    let profileUrl = this.props.profile_image_url ? this.props.profile_image_url : profile_pic;
     let profileImage = <img className="h-100 w4"  src={profileUrl} />;
     let projectList;
 
