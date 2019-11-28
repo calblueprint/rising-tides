b52748a709e7d642eddce88b0d692eb49f1545f8
Claire Lin <clairelin135@gmail.com>
2019-11-27 19:48:23 -0800

    fix cat image

--- app/javascript/components/utils/ProjectCard.jsx
+++ app/javascript/components/utils/ProjectCard.jsx
@@ -6,6 +6,7 @@ class ProjectCard extends React.Component {
   constructor(props) {
     super(props);
     this.goToProject = this.goToProject.bind(this);
+    console.log(props);
   }
 
   goToProject = () => {
@@ -15,7 +16,9 @@ class ProjectCard extends React.Component {
   render() {
     return (
         <div className="proj-card col-item dib ma2 shadow-1" onClick={this.goToProject}>
-          <img src="http://placekitten.com/g/600/300" className="db pic-height w-100 br--top" alt="Photo of a kitten looking menacing."/>
+          <div className="pic-height w-100 br--top db bg-light-gray flex justify-center items-center">
+            <i className="fas fa-image gray f1"/>
+          </div>
           <div className="bt b--white pa3">
             <p className="f4 b lato compact mt3 ma2">
               {this.props.project.title}
