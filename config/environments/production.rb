53c83372e9b9415ae58f5f997bcfde73d1580050
Ethan Lee <hello@ethanl.ee>
2019-11-28 21:27:30 -0800

    Update S3 configuration for new URL scheme

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,12 +107,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-    storage: s3,
+    storage: :s3,
+    preserve_files: true,
+    bucket: ENV.fetch('AWS_BUCKET'),
+    path: ':class/:attachment/:id_partition/:style/:filename',
+    url: ':s3_domain_url',
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.FETCH('AWS_REGION'),
+      s3_region: ENV.fetch('AWS_BUCKET_REGION'),
     }
   }
 end
